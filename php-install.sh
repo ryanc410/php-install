81d40489d723f31b9402f4e61d74f2f687e1db40
Ryan Cook <80371197+ryanc410@users.noreply.github.com>
2022-07-29 07:00:41 -0500

    Update php-install.sh

--- php-install.sh
+++ php-install.sh
@@ -1,15 +1,25 @@
 #!/usr/bin/env bash
 ########################################################
-# NAME               : php-install.sh
-# AUTHOR             : Ryan C.
-# DESCRIPTION        : Install a Version of PHP and set as the default version.
+# NAME                            : php-install.sh
+# AUTHOR                        : Ryan C.
+# DESCRIPTION                : Install a Version of PHP and set as the default version.
 # VERSIONS SUPPORTED : 7.2, 7.4, 8.0, 8.1
-# COMPATABILITY      : Ubuntu Server 20.04 Focal
+# COMPATABILITY            : Ubuntu Server 20.04 Focal
 ########################################################
 reqs=( software-properties-common gnupg2 )
-base_mods=(fpm mysql curl zip xml mbstring cli common)
+base_mods=(fpm mysql cli common)
+current=$(php -v | grep ^PHP | cut -d' ' -f2)
+
+function header() {
+    clear
+    echo "#################################"
+    echo "#         PHP Installer         #"
+    echo "#################################"
+    echo ""
+}
 
 if [[ $EUID -ne 0 ]]; then
+    header
     echo -e "\e[1;31mMust be root or execute with sudo command!\e[0m"
     sleep 3
     exit 1
@@ -17,16 +27,38 @@ fi
 
 cat /etc/lsb-release | grep focal &>/dev/null
 if [[ $? -ne 0 ]]; then
+    header
     echo -e "\e[1;31mThis script is compatible with Ubuntu Server 20.04 only!\e[0m"
     sleep 3
     exit 1
 fi
 
+header
+echo -e "Checking current PHP version.."
+sleep 3
+php -v | grep ^PHP | cut -d' ' -f2 &>/dev/null
+
+if [[ $? -eq 0 ]]; then
+    echo -e "\e[1;32mFound PHP version ${current}..\e[0m"
+    sleep 3
+    switch=true
+else
+echo -e "PHP not found.."
+sleep 3
+    switch=false
+fi
+
 echo -e "Updating Server Repositories..\n"
 apt update &>/dev/null
 
-echo -e "What version of PHP do you want to install?\n"
-read version
+if [[ $switch == true ]]; then
+    echo -e "What version of PHP do you want to switch to?\n"
+    read version
+else
+    echo -e "What version of PHP do you want to install?\n"
+    read version
+fi
+
 case "${version}" in
     7.2)        php_version=7.2;;
     7.4)        php_version=7.4;;
@@ -39,8 +71,17 @@ esac
 
 dpkg -l | grep software-properties-common &>/dev/null
 if [[ $? -ne 0 ]]; then
+    dpkg -l | grep gnupg2 &>/dev/null
+    if [[ $? -ne 0 ]]; then
+        echo -e "Installing required modules..\n"
+        apt install "${reqs[@]}" -y &>/dev/null
+    else
+        echo -e "Installing required modules..\n"
+        apt install software-properties-common -y &>/dev/null
+    fi
+else
     echo -e "Installing required modules..\n"
-    apt install "${reqs[@]}" -y &>/dev/null
+    apt install gnupg2 -y &>/dev/null
 fi
 
 if [[ ! -f /etc/apt/sources.list.d/ondrej-ubuntu-php-focal.list ]]; then
@@ -54,7 +95,7 @@ apt install php"${php_version}" -y &>/dev/null
 
 for mod in "${base_mods[@]}"
 do
-apt install php"${php_version}"-"${mod}" -y &>/dev/null
+    apt install php"${php_version}"-"${mod}" -y &>/dev/null
 done
 
 update-alternatives --set php /usr/bin/php"${php_version}" &>/dev/null
